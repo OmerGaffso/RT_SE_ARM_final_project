bb032fd7338723e87f7225cc29a2cb62398b1878
Omer Penso <penso.omer@gmail.com>
2022-11-25 13:58:29 +0200

    ADC done with counter callback. Start work on Timer test.

--- Omer/Src/ADC.c
+++ Omer/Src/ADC.c
@@ -1,22 +1,35 @@
 #include "RTG.h"
 #include "ADC.h"
 
-uint8_t adc_complete = FALSE;
+/// Count the number of conversions completed, to make sure the conversion
+/// happens at every new loop iteration.
+uint8_t convert_cnt;
 
 uint8_t adc_test(uint8_t iter)
 {
 	uint16_t adc_read = INIT_VALUE;
+	// Enables ADC DMA. Sets it's buffer to adc_read and requests 1 read
 	HAL_ADC_Start_DMA(ADC_HANDLE, (uint32_t *) &adc_read, READ_BUFF);
 
-	while(iter--)
+	// Initiate the read counter to 0
+	convert_cnt = INIT_VALUE;
+
+	for(uint8_t i = 0; i < iter; i++)
 	{
+		// read the value and store it in adc_read
 		HAL_ADC_Start(ADC_HANDLE);
 
-		/// The ADC is not in range.
+		/// check if the ADC read value is out of valid range
 		if ( adc_read < LOWER_BOUND || UPPER_BOUND < adc_read )
 			return FAILURE;
-
 	}
+	if(convert_cnt != iter)
+		return FAILURE;
+
 	return SUCCESS;
 }
 
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	++convert_cnt;
+}
